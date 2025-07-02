fb8e9b171f39f5b3ab26dc4949276780b9001ae6
evorjd17 <163557606+evorjd17@users.noreply.github.com>
2025-07-02 22:18:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '&O8IyHDoI#ulom9-k_yYb98DXsIFB5p8PQeU5OBPC6ysBkJubjARQ' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'O8IyHDoI#ulom9-k_yYb98DXsIFB5p8PQeU5OBPC6ysBkJubjARQ' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? 'H0FGaqrxZgKuKbNlBKdMFklkvgzoBw0qRlQd' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'evorjd17' : process.env.SESSION_NAME, 
 
